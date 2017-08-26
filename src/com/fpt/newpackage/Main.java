8dbd40b2f6f25f24f4a736ba717342836451d391
Dong Vu <dongvdd00244@fpt.edu.vn>
2017-08-26 09:58:28 +0700

    upload project

---
+++ src/com/fpt/newpackage/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fpt.newpackage;
+
+/**
+ *
+ * @author Ducdongpro
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        String names = "Nam, Trường, Dũng, Danh, Tùng, Khánh, Linh, Đông, Chiến, Trường, Ngọc, Toàn, Toản";
+        // Cắt chuỗi 
+        String[] array = names.split(",");
+        // In ra chuỗi
+        for (int j = 0; j < array.length; j++) {
+            System.out.println((j + 1) + ". " + array[j]);
+        }
+    }
+}
